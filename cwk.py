edcd292ba36615b705849cfca48c05f9d66236fb
Amantle Bogacu <u2130894@domain>
2023-02-19 14:44:52 +0000

    login and registration page

--- cwk.py
+++ cwk.py
@@ -74,7 +74,7 @@ def register():
         return redirect("/")
 
     if request.method == "GET":
-        return render_template("registers.html")
+        return render_template("register.html")
 
 
 def makeLogin(username, password):
