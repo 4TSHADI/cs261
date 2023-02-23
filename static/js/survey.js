494fd0aae5f2a3578a3d9bc1b1d49e60ad401df4
Rohit <rohit123@me.com>
2023-02-23 10:26:38 +0000

    survey

--- static/js/survey.js
+++ static/js/survey.js
@@ -1,15 +1,15 @@
 // Wait for the DOM to be ready
 document.addEventListener("DOMContentLoaded", function() {
     // Get all the sliders
-    const sliders = document.querySelectorAll(".slider");
+    const sliders = document.querySelectorAll(".slider-input");
   
     // Loop through each slider
     sliders.forEach(function(slider) {
       // Get the range input element
-      const rangeInput = slider.querySelector(".range-input");
+      const rangeInput = slider.querySelector("input[type='range']");
   
       // Get the value span element
-      const valueSpan = slider.querySelector(".value");
+      const valueSpan = slider.querySelector(".slider-value");
   
       // Set the initial value of the value span
       valueSpan.innerText = rangeInput.value;
@@ -20,3 +20,5 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   });
+
+  
