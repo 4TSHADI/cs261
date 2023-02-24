884b8e2161c11ddb19fff7cded91954c913bd5eb
Shivani Dave <u2045433@dcs.warwick.ac.uk>
2023-02-24 20:11:57 +0000

    Added functionality of 'previous' button and added questions.

--- static/js/ahp_questions.js
+++ static/js/ahp_questions.js
@@ -2,24 +2,131 @@ const questionContainer = document.getElementById("question-container");
 const questionText = document.getElementById("question");
 const answerBtns = document.getElementById("ans-btns");
 const prevBtn = document.getElementById("prev-btn");
+const controls = document.getElementById("controls");
+
+
 
 let questionNum = 0;
 
 const questions = [
     {
-        question: 'What do you find more important?',
+        question: 'Which aspect do you value more in your team project?',
         answers: [
-            {text: 'A'},
-            {text: 'B'},
-            {text: 'Both'}
+            {text: "Size and experience level of team"},
+            {text: "Satisfiability with work environment and project scope"},
+            {text: "Both Equally"}
         ]
     },
     {
-        question: 'What is 9 + 10',
+        question: 'Which aspect do you value more in your team project?',
         answers: [
-            {text: '21'},
-            {text: '19'},
-            {text: 'yes'}
+            {text: "Size and experience level of team"},
+            {text: "Project budget"},
+            {text: "Both Equally"}
+        ]
+    },
+    {
+        question: "Which aspect do you value more in your team project?",
+        answers: [
+            {text: "Size and experience level of team"},
+            {text: "Efficient time management and weekly hours of work"},
+            {text: 'Both Equally'}
+        ]
+    },
+    {
+        question: "Which aspect do you value more in your team project?",
+        answers: [
+            {text: "Size and experience level of team"},
+            {text: "Quality of code and Git pull requests"},
+            {text: "Both Equally"}
+        ]
+    },
+    {
+        question: "Which aspect do you value more in your team project?",
+        answers: [
+            {text: "Size and experience level of team"},
+            {text: "Frequency and effectiveness of communication within team and with stakeholders"},
+            {text: "Both Equally"}
+        ]
+    },
+    {
+        question: 'Which aspect do you value more in your team project?',
+        answers: [
+            {text: "Satisfiability with work environment and project scope"},
+            {text: "Project budget"},
+            {text: "Both Equally"}
+        ]
+    },
+    {
+        question: "Which aspect do you value more in your team project?",
+        answers: [
+            {text: "Satisfiability with work environment and project scope"},
+            {text: "Efficient time management and weekly hours of work"},
+            {text: "Both Equally"}
+        ]
+    },
+    {
+        question: "Which aspect do you value more in your team project?",
+        answers: [
+            {text: "Satisfiability with work environment and project scope"},
+            {text: "Quality of code and Git pull requests"},
+            {text: "Both Equally"}
+        ]
+    },
+    {
+        question: "Which aspect do you value more in your team project?",
+        answers: [
+            {text: "Satisfiability with work environment and project scope"},
+            {text: "Frequency and effectiveness of communication within team and with stakeholders"},
+            {text: "Both Equally"}
+        ]
+    },
+    {
+        question: "Which aspect do you value more in your team project?",
+        answers: [
+            {text: "Project budget"},
+            {text: "Efficient time management and weekly hours of work"},
+            {text: "Both Equally"}
+        ]
+    },
+    {
+        question: "Which aspect do you value more in your team project?",
+        answers: [
+            {text: "Project budget"},
+            {text: "Quality of code and Git pull requests"},
+            {text: "Both Equally"}
+        ]
+    },
+    {
+        question: "Which aspect do you value more in your team project?",
+        answers: [
+            {text: "Project budget"},
+            {text: "Frequency and effectiveness of communication within team and with stakeholders"},
+            {text: "Both Equally"}
+        ]
+    },
+    {
+        question: "Which aspect do you value more in your team project?",
+        answers: [
+            {text: "Efficient time management and weekly hours of work"},
+            {text: "Quality of code and Git pull requests"},
+            {text: "Both Equally"}
+        ]
+    },
+    {
+        question: "Which aspect do you value more in your team project?",
+        answers: [
+            {text: "Efficient time management and weekly hours of work"},
+            {text: "Frequency and effectiveness of communication within team and with stakeholders"},
+            {text: "Both Equally"}
+        ]
+    },
+    {
+        question: "Which aspect do you value more in your team project?",
+        answers: [
+            {text: "Quality of code and Git pull requests"},
+            {text: "Frequency and effectiveness of communication within team and with stakeholders"},
+            {text: "Both Equally"}
         ]
     }
 ]
@@ -29,6 +136,7 @@ nextQuestion();
 prevBtn.addEventListener("click", () => {
     if (questionNum > 0) {
         questionNum--;
+        if (questionNum == 0){prevBtn.classList.add('hide');}
         nextQuestion();
     }
 })
@@ -48,6 +156,9 @@ function removeBtns() {
     while (answerBtns.firstChild) {
         answerBtns.removeChild(answerBtns.firstChild);
     }
+    if (questionNum > 0){
+        prevBtn.classList.remove('hide');
+    }
 }
 
 function displayQuestion(question) {
@@ -59,7 +170,8 @@ function displayQuestion(question) {
         btn.addEventListener("click", () => {
             questionNum++;
             nextQuestion();
-        })
+        });
+    
 
         btn.addEventListener("click", selectAnswer);
         answerBtns.appendChild(btn);
